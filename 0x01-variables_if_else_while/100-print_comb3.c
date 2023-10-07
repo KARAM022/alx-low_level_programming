5eb5b3fad74eabe5802550242d09c55422f8cdca
KARAM022 <my0fav0library@gmail.com>
2023-10-07 20:22:07 +0100

    c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Determines hhh
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main() {
+	for (int i = 0; i < 10; i++) {
+		for (int j = i + 1; j < 10; j++) {
+			putchar(i + '0');
+			putchar(j + '0');
+			if (i != 8 || j != 9) {
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return 0;
+}
