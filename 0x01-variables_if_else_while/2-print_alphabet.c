7e3154d74d40fa486b48cc7135efa8e8f7ffaaf5
KARAM022 <my0fav0library@gmail.com>
2023-10-07 16:36:25 +0100

    c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,17 +2,17 @@
 
 /**
  * main - Determines if a number is positive. negative or zero.
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char c = 'a';
-    while (c <= 'z')
-    {
-        putchar(c);
-        c++;
-    }
-    putchar('\n');
-    return (0);
+	char c = 'a';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
 }
