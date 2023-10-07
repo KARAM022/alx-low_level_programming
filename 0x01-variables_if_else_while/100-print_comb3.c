15b30b33450884c8f5bfc43c84eb6881d3796dc2
KARAM022 <my0fav0library@gmail.com>
2023-10-07 21:02:02 +0100

    c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,7 +5,7 @@
  * Return: Always 0 (Success)
  */
 
-int main(void) 
+int main(void)
 {
 	int fd = 48;
 	int sd;
@@ -13,10 +13,12 @@ int main(void)
 	while (fd < 58)
 	{
 		sd = fd + 1;
-		while (sd < 58) {
+		while (sd < 58)
+		{
 			putchar(fd);
 			putchar(sd);
-			if (fd != 56 || sd != 57) {
+			if (fd != 56 || sd != 57)
+			{
 				putchar(',');
 				putchar(' ');
 			}
