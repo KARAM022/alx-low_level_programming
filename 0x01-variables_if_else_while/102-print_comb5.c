75c3301cd5943be0676cb67bc2012e572324022e
KARAM022 <my0fav0library@gmail.com>
2023-10-08 17:48:11 +0100

    c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - Determines hhh
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int fd = 0,sd;
+
+	while (fd < 100)
+	{
+		sd = fd;
+		while (sd < 100)
+		{
+            if (sd != fd)
+            {
+                putchar((fd / 10) + 48);
+			    putchar((fd % 10) + 48);
+                putchar(' ');
+                putchar((sd / 10) + 48);
+			    putchar((sd % 10) + 48);
+
+                if (fd != 98 || sd != 99)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+                
+            }
+			sd++;
+		}
+		fd++;
+	}
+	putchar('\n');
+	return (0);
+}
