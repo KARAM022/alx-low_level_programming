e86bc3d14dc77a42b9dfcf8ac3d1c40af8adf530
KARAM022 <my0fav0library@gmail.com>
2023-10-10 15:29:18 +0100

    C functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/***/
+
+int main(void)
+{
+    char charlist[] = "_putchar\n";
+    int i;
+
+    for (i = 0; i < 8; i++)
+    {
+        _putchar(charlist[i]);
+    }
+    return (0);
+}
