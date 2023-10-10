b59114e15741b4cefb6b67b852f66c9b9e08dd24
KARAM022 <my0fav0library@gmail.com>
2023-10-10 15:32:14 +0100

    C functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 #include "main.h"
 
-/***/
+/**
+ * main - hhh
+ * _putchar - hhh
+ * Return: 0
+*/
 
 int main(void)
 {
-    char charlist[] = "_putchar\n";
-    int i;
+	char charlist[] = "_putchar\n";
+	int i;
 
-    for (i = 0; i < 9; i++)
-    {
-        _putchar(charlist[i]);
-    }
-    return (0);
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(charlist[i]);
+	}
+	return (0);
 }
