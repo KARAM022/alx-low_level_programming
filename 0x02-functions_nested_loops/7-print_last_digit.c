295be701e15536462b95c9fcc9089b96be0364c7
KARAM022 <my0fav0library@gmail.com>
2023-10-11 03:18:36 +0100

    0x02

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,15 +10,15 @@
 
 int print_last_digit(int n)
 {
-    int b;
-    if (n < 0)
-    {
-        b = -1 * (n % 10);
-    }
-    else
-    {
-        b = n % 10;
-    }
+	int b;
+	if (n < 0)
+	{
+		b = -1 * (n % 10);
+	}
+	else
+	{
+		b = n % 10;
+	}
 	_putchar(b + '0');
 	return (b);
 }
