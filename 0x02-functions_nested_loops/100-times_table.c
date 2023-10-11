f0be775def7c883c40e186cb1a48ca1a1756dc92
KARAM022 <my0fav0library@gmail.com>
2023-10-11 17:24:47 +0100

    0x02

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,41 +7,50 @@
 void print_times_table(int n)
 {
 	int i, j, k;
-    int cont = n + 1;
+	int cont = n + 1;
 
-	for (i = 0; i < cont; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j < cont; j++)
+		for (i = 0; i < cont; i++)
 		{
-			k = j * i;
-			if (j == 0)
+			for (j = 0; j < cont; j++)
 			{
-				_putchar(k + '0');
-			}
+				k = j * i;
+				if (j == 0)
+				{
+					_putchar(k + '0');
+				}
 
-			if (k < 10 && j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			} else if (k >= 10 && k < 99)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			} else if (k >= 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 100) + '0');
-				_putchar(((k / 10) % 10) + '0');
-				_putchar((k % 10) + '0');
-			} 
+				if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				}
+				else if (k >= 10 && k < 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+				else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
+				}
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
+	}
+	else
+	{
+		return (0);
 	}
 }
