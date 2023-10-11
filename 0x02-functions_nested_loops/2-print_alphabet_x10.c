8538022cfb66403ca44ae90353b790e72218e76a
KARAM022 <my0fav0library@gmail.com>
2023-10-11 01:46:10 +0100

    0x02

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,15 +7,15 @@
 
 void print_alphabet_x10(void)
 {
-    char chara_z[] = "abcdefghijklmnopqrstuvwxyz\n";
-    int i;
-    int j;
+	char chara_z[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int i;
+	int j;
 
-    for (j = 0; j < 10; j++)
-    {
-        for (i = 0; i < 28; i++)
-        {
-            _putchar(chara_z[i]);
-        }
-    }
+	for (j = 0; j < 10; j++)
+	{
+		for (i = 0; i < 28; i++)
+		{
+			_putchar(chara_z[i]);
+		}
+	}
 }
