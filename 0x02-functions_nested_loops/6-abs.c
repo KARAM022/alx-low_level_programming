c186fc988fa170a385520c190cbb01239edd488a
KARAM022 <my0fav0library@gmail.com>
2023-10-11 02:49:47 +0100

    0x02

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - hhhh
+ * @n: hhhh
+ * Return: n or -n
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
