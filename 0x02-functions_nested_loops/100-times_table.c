22673134d3d192084f1874769020606a3f7782bd
KARAM022 <my0fav0library@gmail.com>
2023-10-11 17:30:02 +0100

    0x02

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,43 @@
 #include "main.h"
 
 /**
- * print_times_table - hhhh
- * @n: fff
+ * print_times_table - Print a times table from 0 to n.
+ * @n: The value up to which the times table should be printed.
  */
 void print_times_table(int n)
 {
-	int i, j, k;
-	int cont = n + 1;
+    int i, j, k;
 
-	if (n >= 0 && n <= 15)
-	{
-		for (i = 0; i < cont; i++)
-		{
-			for (j = 0; j < cont; j++)
-			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				}
+    if (n < 0 || n > 15)
+        return;
 
-				if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				}
-				else if (k >= 10 && k < 99)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-				else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-			}
-			_putchar('\n');
-		}
-	}
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= n; j++)
+        {
+            k = i * j;
+            if (j == 0)
+            {
+                _putchar('0');
+            }
+            else
+            {
+                _putchar(',');
+                _putchar(' ');
+                if (k < 10)
+                {
+                    _putchar(' ');
+                    _putchar(' ');
+                }
+                else if (k < 100)
+                {
+                    _putchar(' ');
+                }
+                _putchar((k / 100) + '0');
+                _putchar((k / 10 % 10) + '0');
+                _putchar((k % 10) + '0');
+            }
+        }
+        _putchar('\n');
+    }
 }
