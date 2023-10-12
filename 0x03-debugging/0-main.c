573bac9c5f22f9d660abd033fc272514dd14c825
KARAM022 <my0fav0library@gmail.com>
2023-10-12 15:22:09 +0100

    0x03

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include "main.h"
 
+/**
+ * main - hhhh
+ * Return: 0
+*/
+
 int main(void)
 {
-    int i = 0;
+	int i = 0;
 
-    positive_or_negative(i);
-    return (0);
+	positive_or_negative(i);
+	return (0);
 }
