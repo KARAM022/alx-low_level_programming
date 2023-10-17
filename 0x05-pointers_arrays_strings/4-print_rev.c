4654a1e9f29be306f14bb98fec380937fcfb612a
KARAM022 <my0fav0library@gmail.com>
2023-10-17 14:50:22 +0100

    0x05

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - hh
+ * @s: ff
+ * Return: nothing
+*/
+
+void print_rev(char *s)
+{
+	int len = _strlen(*s),i;
+
+	for (i = len; i = -1; i--)
+	{
+		putchar(s[i]);
+	}
+	putchar('\n');
+}
