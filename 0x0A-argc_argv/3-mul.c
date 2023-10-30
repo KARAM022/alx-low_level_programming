54e597ab25e03462a89b566fdb19f3ff24aeda9a
KARAM022 <my0fav0library@gmail.com>
2023-10-30 21:19:01 +0100

    0x0A

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,11 +16,11 @@ int main(int argc, char **argv)
 	{
 		s = atoi(argv[argc - 1]) * atoi(argv[argc - 2]);
 		printf("%d", s);
-		return (0);
 	}
 	else
 	{
 		printf("Error\n");
 		return (1);
 	}
+	return (0);
 }
