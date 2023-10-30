8dcec42c163baf385b0c7e5a474d2392550055ff
KARAM022 <my0fav0library@gmail.com>
2023-10-30 20:09:06 +0100

    0x0A

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
-/***/
+/**
+ * main - khkhkh 
+ * @argc: khhkh
+ * @argv: kkfkf
+ * Return: 0
+*/
 
 int main(int argc, char **argv)
 {
-    printf("%s\n", argv[argc-1]);
-    return(0);
+	printf("%s\n", argv[argc-1]);
+	return(0);
 }
