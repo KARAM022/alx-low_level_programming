af3dd14714c1abe2ea438dc2aa0190b1180a1f0f
KARAM022 <my0fav0library@gmail.com>
2023-11-03 20:44:19 +0100

    0x0C

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,33 +20,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-
 	while (s1[s1_len] != '\0')
 		s1_len++;
 	while (s2[s2_len] != '\0')
 		s2_len++;
-
-
 	if (n >= s2_len)
 		n = s2_len;
-
-
 	concatenated = malloc(s1_len + n + 1);
-	
 	if (concatenated == NULL)
 		return (NULL);
-
-
 	for (i = 0; i < s1_len; i++)
 		concatenated[i] = s1[i];
-
-
 	for (j = 0; j < n; j++, i++)
 		concatenated[i] = s2[j];
-
-
 	concatenated[i] = '\0';
-
 	return (concatenated);
 }
