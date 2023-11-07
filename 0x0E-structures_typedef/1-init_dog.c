375eef2102076f99659af826076e227ca9abfb38
KARAM022 <my0fav0library@gmail.com>
2023-11-07 03:39:58 +0100

    0x0E

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "dog.h"
+
+
+/**
+ * init_dog - khkhhkhkhk
+ * @d: khkhhkhkhk
+ * @name: khkhhkhkhk
+ * @age: khkhhkhkhk
+ * @owner: khkhhkhkhk
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+	
+}
