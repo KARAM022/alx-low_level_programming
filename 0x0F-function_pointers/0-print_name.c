03a557bb053f3ac94113fb907cbb57d64cdcc4d1
KARAM022 <my0fav0library@gmail.com>
2023-11-08 21:25:10 +0100

    0x0F

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -8,9 +8,9 @@ void print_name(char *name, void (*f)(char *))
     f(name);
 }
 
-/*int main(void) {
+int main(void) {
     print_name("Bob", print_name_as_is);
     print_name("Bob Dylan", print_name_uppercase);
     printf("\n");
     return (0);
-}*/
+}
