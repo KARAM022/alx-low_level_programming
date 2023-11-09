a48d4501b7ab7c9013173d4c2116e59a1c0c6548
KARAM022 <my0fav0library@gmail.com>
2023-11-09 05:48:32 +0100

    0x0F

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-#include "main.h"
+#include "function_pointers.h"
 
-/***/
+/**
+ * print_name - khkhkh
+ * @name: khkhkh
+ * @f: khkhkh
+ **/
+
 
 void print_name(char *name, void (*f)(char *))
 {
-    f(name);
-}
+	if (name != NULL && f != NULL)
+		f(name);
 
-int main(void) {
-    print_name("Bob", print_name_as_is);
-    print_name("Bob Dylan", print_name_uppercase);
-    printf("\n");
-    return (0);
 }
