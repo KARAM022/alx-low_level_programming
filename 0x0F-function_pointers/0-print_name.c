4ff20765ba2024155cbd8b43f5b060eacf472605
KARAM022 <my0fav0library@gmail.com>
2023-11-09 05:54:03 +0100

    0x0F

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,11 +2,14 @@
 #include "function_pointers.h"
 
 /**
- * 
+ * print_name - khkhkh
+ * @name: khkhkh
+ * @f: khkhkh
+ * Return: khkh
  **/
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
+	if (name!=NULL && f != NULL)
 		f(name);
 }
