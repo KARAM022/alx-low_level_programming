e94b924b4951658b2220a365c4cd8b3361113ca6
KARAM022 <my0fav0library@gmail.com>
2023-11-17 02:42:21 +0100

    0x12

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,35 +1,35 @@
 #include "variadic_functions.h"
-	#include <stdio.h>
-	#include <stdarg.h>
+#include <stdio.h>
+#include <stdarg.h>
 
 
-	/**
-	 * print_numbers - hkkhkhk
-	 * @separator: hkkhkhk
-	 * @n: hkkhkhk
-	 * @...: hkkhkhk
-	 */
-	void print_numbers(const char *separator, const unsigned int n, ...)
+/**
+ * print_numbers - hkkhkhk
+ * @separator: hkkhkhk
+ * @n: hkkhkhk
+ * @...: hkkhkhk
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list nums;
+	unsigned int index;
+
+
+	va_start(nums, n);
+
+
+	for (index = 0; index < n; index++)
 	{
-		va_list nums;
-		unsigned int index;
+		printf("%d", va_arg(nums, int));
 
 
-		va_start(nums, n);
-
-
-		for (index = 0; index < n; index++)
-		{
-			printf("%d", va_arg(nums, int));
-
-
-			if (index != (n - 1) && separator != NULL)
-				printf("%s", separator);
-		}
-
-
-		printf("\n");
-
-
-		va_end(nums);
+		if (index != (n - 1) && separator != NULL)
+			printf("%s", separator);
 	}
+
+
+	printf("\n");
+
+
+	va_end(nums);
+}
