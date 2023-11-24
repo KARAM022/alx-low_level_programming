cc3269b46eb8e9ffa21d1bd8085caa6c5bb42f04
KARAM022 <my0fav0library@gmail.com>
2023-11-24 01:19:03 +0100

    0x14

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - khkhkhk
+ * @b: khkhkhk
+ *
+ * Return: khkhkhk
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int a;
+	unsigned int n;
+
+	n = 0;
+	if (!b)
+		return (0);
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		if (b[a] != '0' && b[a] != '1')
+			return (0);
+	}
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		n <<= 1;
+		if (b[a] == '1')
+			n += 1;
+	}
+	return (n);
+}
